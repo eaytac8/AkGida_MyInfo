36afc5aeae67ce65ffeaaf166392488accdea634
eaytac8 <elif.aytc@hotmail.com>
2018-11-06 10:11:47 +0300

    v4.1

--- AkGida_MyInfo/Models/MyInfoDB.Designer.cs
+++ AkGida_MyInfo/Models/MyInfoDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\elif.aytac\source\repos\AkGida_MyInfo\AkGida_MyInfo\Models\MyInfoDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\admelif\Downloads\AkGida_MyInfo\AkGida_MyInfo\Models\MyInfoDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
